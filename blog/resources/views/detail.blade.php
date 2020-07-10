6f5e74f28d458f1a57f2fb7ea68d57ee952e2a37
imaaronath <aaron_irwanto@yahoo.com>
2020-07-10 12:07:54 +0700

    ambil blog

--- blog/resources/views/detail.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('master.welcome4')
-@section('content')
-  <div class="container" style="margin-top:150px">
-    <h1>disini tempat menampilkan detail data</h1>
-  </div>
-@endsection
--- blog/resources/views/edit.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('master.welcome4')
-@section('content')
-  <div class="container" style="margin-top:150px">
-    <h1>disini tempat untuk mengedit data</h1>
-  </div>
-@endsection
